d8d5eb7b1605b92e8fa89b154dcb923f228117be
gdedi001 <gabrielfdediego@gmail.com>
2020-05-20 18:49:18 -0400

    feat: add logic to enable dart on pinch
    Dart gameobject will now appear in hand of user when index finger is pinched.

---
+++ Assets/Scripts/HandsInteractionManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsInteractionManager : MonoBehaviour
+{
+    private OVRHand hand;
+    private string currentHand;
+
+    // Gameobject used as dart in current hand
+    public GameObject Dart;
+
+    [SerializeField]
+    private float minFingerPinchStrength = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hand = gameObject.GetComponent<OVRHand>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LoadHandDart();
+    }
+
+    void FixedUpdate()
+    {
+        // if hand tracking is reliable and index finger is pinched. Display dart
+        if (isHandTrackingReliable())
+        {
+
+        }
+        // upon releasing pinch, add gravity and 
+    }
+
+    private bool isHandTrackingReliable()
+    {
+        OVRHand.TrackingConfidence confidence = hand.GetFingerConfidence(OVRHand.HandFinger.Index);
+
+        if (confidence == OVRHand.TrackingConfidence.High)
+            return true;
+        else
+            return false;
+    }
+
+    private void LoadHandDart()
+    {
+        bool isIndexFingerPinching = hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+        float ringFingerPinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+
+        if (isIndexFingerPinching
+            && ringFingerPinchStrength > 0.7f)
+        {
+            Debug.Log("!!! Dart Active");
+            Dart.SetActive(true);
+        }
+        else
+        {
+            Dart.SetActive(false);
+            Debug.Log("??? Dart De-Activated");
+        }
+    }
+}
